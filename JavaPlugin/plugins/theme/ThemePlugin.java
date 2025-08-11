e5b127b15894a4526d1f77b044292ca741a51da2
Ashlesh Morghade <ashleshmorghade@gmail.com>
2025-08-11 10:17:31 +0530

    Added Theme Option

--- JavaPlugin/plugins/theme/ThemePlugin.java
+++ JavaPlugin/plugins/theme/ThemePlugin.java
@@ -27,26 +27,26 @@ public class ThemePlugin implements Plugin {
 
         Font currentFont = editor.getFont();
         switch (choice) {
-            case 0: // Dark Theme
+            case 0: 
                 editor.setBackground(new Color(40, 44, 52));
                 editor.setForeground(Color.WHITE);
                 editor.setCaretColor(Color.WHITE);
                 editor.setSelectionColor(new Color(80, 80, 120));
                 break;
-            case 1: // Light Theme
+            case 1:
                 editor.setBackground(Color.WHITE);
                 editor.setForeground(Color.BLACK);
                 editor.setCaretColor(Color.BLACK);
                 editor.setSelectionColor(new Color(180, 200, 250));
                 break;
-            case 2: // Increase Font Size
+            case 2:
                 editor.setFont(new Font(currentFont.getName(), currentFont.getStyle(), Math.min(currentFont.getSize()+2, 40)));
                 break;
-            case 3: // Decrease Font Size
+            case 3:
                 editor.setFont(new Font(currentFont.getName(), currentFont.getStyle(), Math.max(currentFont.getSize()-2, 10)));
                 break;
             default:
-                // Do nothing
+                
         }
     }
 }
